7e5b850d69f4d631f2d9663c21e5f83aa5c46c66
thespace83 <biometer2020@gmail.com>
2026-05-24 15:53:38 +0300

    Добавил deltatime

--- player.py
+++ player.py
@@ -1,27 +1,32 @@
 import pygame as pg
 
+
 class Player:
     """ Класс игрока.
     """
+
     def __init__(self, speed: int):
-        self.x = 0
-        self.y = 0
+        """
+        Создание класса.
+        :param speed: Скорость игрока.
+        """
+        self.pos: pg.Vector2 = pg.Vector2(0, 0)
         self.speed = speed
 
-    def logic(self):
+    def logic(self, dt):
         """ Обработка нажатий на кнопки.
         """
         keys = pg.key.get_pressed()
-        if (keys[pg.K_d]):
-            self.x += 1
-        if (keys[pg.K_a]):
-            self.x -= 1
-        if (keys[pg.K_s]):
-            self.y += 1
-        if (keys[pg.K_w]):
-            self.y -= 1
+        if keys[pg.K_d]:
+            self.pos.x += self.speed * dt
+        if keys[pg.K_a]:
+            self.pos.x -= self.speed * dt
+        if keys[pg.K_s]:
+            self.pos.y += self.speed * dt
+        if keys[pg.K_w]:
+            self.pos.y -= self.speed * dt
 
     def draw(self, surface):
         """Отрисовывать игрока на карте.
         """
-        pg.draw.circle(surface, (255, 0, 0), (self.x, self.y), 30)
+        pg.draw.circle(surface, (255, 0, 0), self.pos, 30)
