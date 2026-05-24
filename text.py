1af145e1e0dd9b9b71b437a0f115a20f825fe110
thespace83 <biometer2020@gmail.com>
2026-05-24 16:11:41 +0300

    Добавить комментарии.

--- text.py
+++ text.py
@@ -1,12 +1,23 @@
+"""Файл с классом текста.
+"""
 import pygame as pg
 
 
 class Text:
+    """Класс отрисовывающий текст.
+    """
+
     def __init__(self,
                  pos: pg.Vector2,
                  text: str,
                  color: tuple[int, int, int],
                  size: int):
+        """Создание класса.
+        :param pos:  Позиция картинки.
+        :param text: Текст картинки.
+        :param color: Цвет картинки.
+        :param size: Размер картинки.
+        """
         self.pos: pg.Vector2 = pos
         self.text: str = text
         self.color: tuple[int, int, int] = color
@@ -16,4 +27,8 @@ class Text:
         self.texture = self.font.render(text, True, color)
 
     def draw(self, surface: pg.Surface):
+        """Отрисовка картинки.
+        :param surface: Поверхность на которой будет отрисована картинка.
+        :return:
+        """
         surface.blit(self.texture, self.pos)
