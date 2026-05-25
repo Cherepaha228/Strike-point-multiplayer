92db1356388fd6a3bf53eb8dab0f8eac95d1a268
Cherepaha228 <spacemishadiachkov@gmail.com>
2026-05-25 15:24:10 +0300

    функция __init__

---
+++ gun.py
@@ -0,0 +1,9 @@
+class Gun:
+    def __init__(self, is_down, pos, texture, gun_damage, gun_weight):
+        self.is_down = is_down
+        self.pos = pos
+        self.texture = texture
+        self.gun_damage = gun_damage
+        self.gun_weight = gun_weight
+
+
